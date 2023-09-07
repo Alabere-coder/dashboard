e864efa20a14a02d3bdb336feae07774d7db371c
Alabere-coder <alabere4real247@gmail.com>
2023-09-07 15:39:08 +0100

    landing page initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/navbar'
+import HeaderPage from './components/Header'
+
+function App() {
+
+  return (
+    <div>
+        <Navbar />
+        <HeaderPage />
+    </div>
+      
+  )
+}
+
+export default App
